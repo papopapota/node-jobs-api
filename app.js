9479f84f358f4c979303d42fa43e6fe6b3976d36
Daniel Olivares <76667767+papopapota@users.noreply.github.com>
2025-12-27 11:06:05 -0500

    se comento helmet

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ swaggerDocument.servers = [
   },
 ];
 //security packages//////
-//const helmet = require('helmet');
+const helmet = require('helmet');
 const cors = require('cors');
 const xss = require('xss-clean');
 const rateLimiter = require('express-rate-limit');
@@ -45,7 +45,7 @@ app.get('/', (req, res) => {
   `);
 });
 app.use(express.json());
-app.use(helmet());
+//app.use(helmet());
 app.use(cors());
 app.use(xss());
 app.use(
