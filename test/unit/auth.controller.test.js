fd9a56f929f5964cc4137c7795d750541e5bda8d
Daniel Olivares <76667767+papopapota@users.noreply.github.com>
2026-01-23 11:29:16 -0500

    se mejoro las las pruebas untiarias para auth controller

--- test/unit/auth.controller.test.js
+++ test/unit/auth.controller.test.js
@@ -1,8 +1,19 @@
 const { register, login } = require('../../controllers/auth');
 const httpMocks = require('node-mocks-http');
 const User = require('../../models/User');
+const { mock } = require('jest-mock-extended');
 
 jest.mock('../../models/User');
+
+const mockRequest = (body) => {
+    return httpMocks.createRequest({
+        body
+    });
+}
+
+const mockResponse = (body) => {
+    return httpMocks.createResponse();
+}
 
 describe('Auth controller - Unit Test', () => {
     afterEach(() => {
@@ -11,70 +22,59 @@ describe('Auth controller - Unit Test', () => {
 
     describe('Register', () => {
         it('should create a user and return token', async () => {
-            const req = httpMocks.createRequest({
-                body: {
-                    "name": "Daniel",
-                    "email": "test@gmail.com",
-                    "password": "secret"
-                }
-            })
-            const res = httpMocks.createResponse();
+            const req = mockRequest({
+                "name": "Daniel",
+                "email": "test@gmail.com",
+                "password": "secret"
+            });
+            const res = mockResponse();
             User.create.mockResolvedValue({
-                getName: () => 'Daniel',
-                createJWT: () => 'fake-jwt-token'
+                getName: jest.fn().mockReturnValue('Daniel'),
+                createJWT: jest.fn().mockReturnValue('fake-jwt-token')
             });
 
             await register(req, res);
-
+            expect(User.create).toHaveBeenCalledWith(req.body);
             expect(res.statusCode).toBe(201);
-            expect(User.create).toHaveBeenCalledWith({
-                name: "Daniel",
-                email: "test@gmail.com",
-                password: "secret"
-            })
-
             const data = res._getJSONData();
             expect(data).toEqual({
-                user: { name: 'Daniel' }, token: 'fake-jwt-token'
+                user: { name: 'Daniel' },
+                token: 'fake-jwt-token'
             });
         });
-        it('should throw an error if email was not provided', async () => {
-            const req = httpMocks.createRequest({
-                body: {
-                    "name": "Daniel",
-                    "email": "",
-                    "password": "secret"
-                }
-            })
-            const res = httpMocks.createResponse();
-            User.create.mockResolvedValue({
-                
+        it('should throw an error if user credentials fails', async () => {
+            const req = mockRequest({
+                "name": "Daniel",
+                "email": "",
+                "password": ""
             });
-            return await expect(register(req, res)).rejects.toThrow('Please provide email');
+            const res = mockResponse();
+            User.create.mockRejectedValue(new Error('Validation Error'));
+            await expect(register(req, res)).rejects.toThrow('Validation Error');
         });
     })
+
     describe('Login', () => {
         it('should login user with valid credentials  ', async () => {
-            const req = httpMocks.createRequest({
-                body: {
-                    "email": "test@gmail.com",
-                    "password": "secret"
-                }
+            const req = mockRequest({
+                "email": "test@gmail.com",
+                "password": "secret"
             });
 
-            const res = httpMocks.createResponse();
-            User.findOne.mockResolvedValue({
-                getName: () => 'Daniel',
-                createJWT: () => 'fake-jwt-token',
+            const res = mockResponse();
+            const userMock = {
+                getName: jest.fn().mockReturnValue('Daniel'),
+                createJWT: jest.fn().mockReturnValue('fake-jwt-token'),
                 comparePassword: jest.fn().mockResolvedValue(true)
-            });
+            }
+            User.findOne.mockResolvedValue(userMock);
             await login(req, res);
-
-            expect(res.statusCode).toBe(200);
             expect(User.findOne).toHaveBeenCalledWith({
                 email: "test@gmail.com"
-            })
-
+            });
+            expect(userMock.comparePassword).toHaveBeenCalledWith("secret");
+            expect(userMock.createJWT).toHaveBeenCalledWith();
+            expect(res.statusCode).toBe(200);
             const data = res._getJSONData();
             expect(data).toEqual({
                 user: { name: 'Daniel' }, token: 'fake-jwt-token'
@@ -82,14 +82,12 @@ describe('Auth controller - Unit Test', () => {
         });
 
         it('should throw error if user not found', async () => {
-            const req = httpMocks.createRequest({
-                body: {
-                    "email": "test@gmail.com",
-                    "password": "secret"
-                }
+            const req = mockRequest({
+                "email": "test@gmail.com",
+                "password": "secret"
             });
 
-            const res = httpMocks.createResponse();
+            const res = mockResponse();
             User.findOne.mockResolvedValue(null);
             await expect(login(req, res)).rejects.toThrow('Invalid Credentials.');
             expect(User.findOne).toHaveBeenCalledWith({
@@ -98,17 +96,14 @@ describe('Auth controller - Unit Test', () => {
         });
 
         it('should throw error if password is incorrect', async () => {
-            const req = httpMocks.createRequest({
-                body: {
-                    "email": "test@gmail.com",
-                    "password": "wrongpassword"
-                }
+            const req = mockRequest({
+                "email": "test@gmail.com",
+                "password": "wrongpassword"
             });
 
-            const res = httpMocks.createResponse();
+            const res = mockResponse();
             User.findOne.mockResolvedValue({
                 comparePassword: jest.fn().mockResolvedValue(false)
-
             });
             await expect(login(req, res)).rejects.toThrow('Invalid Credentials.');
             expect(User.findOne).toHaveBeenCalledWith({
@@ -116,18 +111,6 @@ describe('Auth controller - Unit Test', () => {
             })
         });
 
-        it('should throw error if password and email were not provided', async () => {
-            const req = httpMocks.createRequest({
-                body: {
-                    "email": "",
-                    "password": ""
-                }
-            });
-            const res = httpMocks.createResponse();
-            await expect(login(req, res)).rejects.toThrow('Please provide email and password');
-            expect(User.findOne).not.toHaveBeenCalled();
-
-        });
     })
 
 })
